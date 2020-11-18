c8fe2fe5622365ead1e2d31cf2518c979911014a
Greg Hawthorne <ghawthorne@me.com>
2020-11-18 14:34:36 +0100

    percent convert

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
-
 for question in question_data:
     question_text = question["text"]
-    question_answer = question["text"]
-    new_question = Question(q_text=question_text, q_answer=question_answer)
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
-
-print(question_bank[0].answer)
 
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
 
 # class User:
 #     def __init__(self, user_id, username):
