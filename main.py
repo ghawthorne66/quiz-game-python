00db6b8d20c78b05ba94e00eb27f023e6a9a124f
Greg Hawthorne <ghawthorne@me.com>
2020-11-18 13:09:16 +0100

    add question Class

---
+++ main.py
@@ -0,0 +1,38 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["text"]
+    new_question = Question(q_text=question_text, q_answer=question_answer)
+    question_bank.append(new_question)
+
+print(question_bank)
+
+
+# class User:
+#     def __init__(self, user_id, username):
+#         self.id = user_id
+#         self.name = username
+#         self.followers = 0
+#         self.following = 0
+#
+#     def follow(self, user):
+#         user.followers += 1
+#         self.following += 1
+#
+#
+# user_1 = User("001", "Chewbacca")
+# print(user_1.id, user_1.name)
+#
+# user_2 = User("002", "Darth Vader")
+# print(user_2.id, user_2.name)
+#
+# user_1.follow(user_2)
+# print(f"user 1 has {user_1.followers} followers")
+# print(user_1.following)
+# print(f"user 2 has {user_2.followers} followers")
+# print(user_2.following)
+
